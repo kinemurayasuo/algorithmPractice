4e35de11d5a8dd1944e69cfc99186ac16fa942a3
kinemurayasuo <fkdlzkf19@gmail.com>
2022-05-26 14:22:51 +0900

    입력한 값만큼 1씩 증가 완성

---
+++ src/practicloof/Nclick.java
@@ -0,0 +1,24 @@
+package practicloof;
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Nclick 
+{
+	public static void main(String[] args) throws IOException 
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int num = Integer.parseInt(br.readLine());
+		
+		// 증가식
+		for(int i = 1; i <= num; i++) 
+		{
+			System.out.println(i);
+		}
+		
+		
+	
+	}
+}
